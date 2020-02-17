9eb7506c97ff5839d92f46ff2295c0d56c6e8c76
Jędrzej <jedrzejb123@gmail.com>
2020-02-17 21:56:31 +0100

    add function square and rectangle

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-function clicked() {
-    var input_value = document.getElementById('data').value;
-    document.getElementById('display').innerHTML = input_value * input_value;
-    console.log(input_value);
-}
 
-document.getElementById('btn').addEventListener('click', clicked);
+function square() {
+    let a = document.querySelector("#side").value;
+    if (a == 0) {
+        document.querySelector(".square-result").innerHTML = "Bok nie może być równy 0";
+    }
+    else if (a < 0) {
+        document.querySelector(".square-result").innerHTML = "Bok nie może być mniejszy od zera";
+    }
+    else {
+        let answer = a * a;
+        document.querySelector(".square-result").innerHTML = answer;
+    }
+}
+function rectangle() {
+    let a = document.querySelector("#side-a").value;
+    let b = document.querySelector("#side-b").value;
+    if (a == 0 || b == 0) {
+        document.querySelector(".rectangle-result").innerHTML = "Jeden z boków jest równy zero";
+    }
+    else if (a < 0 || b < 0) {
+        document.querySelector(".rectangle-result").innerHTML = "Jeden z boków jest mniejszy od zera";
+    }
+    else {
+        let answer = a * b;
+        document.querySelector(".rectangle-result").innerHTML = answer;
+    }
+
+}
