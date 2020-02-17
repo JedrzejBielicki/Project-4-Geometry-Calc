63be848b9c4a486cfac18298c7808f9ed48ab166
Jędrzej <jedrzejb123@gmail.com>
2020-02-17 22:40:20 +0100

    add function triangle add function trapezoid(need fix)

--- app.js
+++ app.js
@@ -25,5 +25,38 @@ function rectangle() {
         let answer = a * b;
         document.querySelector(".rectangle-result").innerHTML = answer;
     }
+}
+function triangle() {
+    let a = document.querySelector("#triangle-side").value;
+    let b = document.querySelector("#height").value;
+    if (a == 0 || b == 0) {
+        document.querySelector(".triangle-result").innerHTML = "Jedena z długości jest równa zero";
+    }
+    else if (a < 0 || b < 0) {
+        document.querySelector(".triangle-result").innerHTML = "Jedena z długości jest mniejsza od zera";
+    }
+    else {
+        let answer = 0.5 * a * b;
+        document.querySelector(".triangle-result").innerHTML = answer;
+    }
+
+}
+function trapezoid() {
+    let a = document.querySelector("#trapezoid-side-longer").value;
+    let b = document.querySelector("#trapezoid-side-shorter").value;
+    let c = document.querySelector("#trapezoid-height").value;
+    console.log(a)
+    console.log(b)
+    console.log(c)
+    // if (a == 0 || b == 0 || c == 0) {
+    //     document.querySelector(".trapezoid-result").innerHTML = "Jedena z długości jest równa zero";
+    // }
+    // else if (a < 0 || b < 0 || c < 0) {
+    //     document.querySelector(".trapezoid-result").innerHTML = "Jedena z długości jest mniejsza od zera";
+    // }
+
+    let answer = (a + b);
+    document.querySelector(".trapezoid-result").innerHTML = answer;
+
 
 }
