9c79dbe7a6b9b228e55ef907becfcb70e9ebb0bc
Jędrzej <jedrzejb123@gmail.com>
2020-02-18 21:42:14 +0100

    finish two-dimention section

--- app.js
+++ app.js
@@ -45,18 +45,43 @@ function trapezoid() {
     let a = document.querySelector("#trapezoid-side-longer").value;
     let b = document.querySelector("#trapezoid-side-shorter").value;
     let c = document.querySelector("#trapezoid-height").value;
-    console.log(a)
-    console.log(b)
-    console.log(c)
-    // if (a == 0 || b == 0 || c == 0) {
-    //     document.querySelector(".trapezoid-result").innerHTML = "Jedena z długości jest równa zero";
-    // }
-    // else if (a < 0 || b < 0 || c < 0) {
-    //     document.querySelector(".trapezoid-result").innerHTML = "Jedena z długości jest mniejsza od zera";
-    // }
-
-    let answer = (a + b);
-    document.querySelector(".trapezoid-result").innerHTML = answer;
-
-
+    let d = parseInt(a);
+    let e = parseInt(b);
+    if (a == 0 || b == 0 || c == 0) {
+        document.querySelector(".trapezoid-result").innerHTML = "Jedena z długości jest równa zero";
+    }
+    else if (a < 0 || b < 0 || c < 0) {
+        document.querySelector(".trapezoid-result").innerHTML = "Jedena z długości jest mniejsza od zera";
+    }
+    else {
+        let answer = (d + e) / 2 * c;
+        document.querySelector(".trapezoid-result").innerHTML = answer;
+    }
+}
+function rhombus() {
+    let a = document.querySelector("#rhombus-side").value;
+    let b = document.querySelector("#rhombus-height").value;
+    if (a == 0 || b == 0) {
+        document.querySelector(".rhombus-result").innerHTML = "Jedena z wartości jest równy zero";
+    }
+    else if (a < 0 || b < 0) {
+        document.querySelector(".rhombus-result").innerHTML = "Jedena z wartości jest mniejszy od zera";
+    }
+    else {
+        let answer = a * b;
+        document.querySelector(".rhombus-result").innerHTML = answer;
+    }
+}
+function circle() {
+    let a = document.querySelector("#circle-radius").value;
+    if (a == 0) {
+        document.querySelector(".circle-result").innerHTML = "Promień nie może być równy 0";
+    }
+    else if (a < 0) {
+        document.querySelector(".circle-result").innerHTML = "Promień nie może być mniejszy od zera";
+    }
+    else {
+        let answer = 3.1415 * (a * a);
+        document.querySelector(".circle-result").innerHTML = answer;
+    }
 }
